3a9fc440880540a7b3d0e6ce89af667a6526cb70
Marek Stefanowski <maza256@gmail.com>
2025-01-13 17:21:45 +0000

    Configure date range to work in an expected manner

--- app/dashboard/holidays/page.tsx
+++ app/dashboard/holidays/page.tsx
@@ -1,108 +1,34 @@
 "use client"
 
 import * as React from "react"
-import {format} from "date-fns"
-import {CalendarIcon} from "lucide-react"
 
-import {cn} from "@/lib/utils"
-import {Button} from "@/components/ui/button"
 import {Calendar} from "@/components/ui/calendar"
-import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
-} from "@/components/ui/popover"
-import {DateRange} from "react-day-picker";
 
-// export default function Page() {
-//     const [date, setDate] = React.useState<(DateRange)>({from: undefined, to:undefined});
-//
-//     const handleSelect = (daySelection: DateRange | undefined) => {
-//         if (daySelection) {
-//             setDate(daySelection);
-//         }
-//
-//         console.log(date)
-//         console.log(daySelection)
-//     };
-// const handleSelect = (daySelection: DateRange | undefined) => {
-//     console.log(daySelection);
-//     if(date.from === undefined) {
-//         console.log("Setting to")
-//         setDate({from: daySelection?.from, to: undefined});
-//     } else if (date.to === undefined) {
-//         console.log("Setting from")
-//         setDate({from: date.from, to: daySelection?.to});
-//     } else {
-//         console.log("Resetting it")
-//         setDate({from: daySelection?.from, to: undefined});
-//     }
-// }
-// return (
-
-
+interface DateRange {
+    from: Date | undefined;
+    to: Date | undefined;
+}
 
 export default function Page() {
-    const [date, setDate] = React.useState<DateRange>({from: undefined, to: undefined});
+    const [selectedRange, setSelectedRange] = React.useState({ from: undefined, to: undefined });
 
-    const handleSelect = (daySelection: DateRange | undefined) => {
-    console.log(daySelection);
-    if(date.from === undefined) {
-        console.log("Setting to")
-        setDate({from: daySelection?.from, to: undefined});
-    } else if (date.to === undefined) {
-        console.log("Setting from")
-        setDate({from: date.from, to: daySelection?.to});
-    } else {
-        console.log("Resetting it")
-        setDate({from: undefined, to:undefined})
-        setDate({from: daySelection?.from, to: undefined});
+    const handleRangeChange = (nextRange: { from: Date; to: Date; }, selectedDay: any) => {
+        setSelectedRange((range) => {
+            if (range?.from && range?.to) return {from: selectedDay};
+            if (!range?.from && !range?.to) return {from: selectedDay};
+            return nextRange;
+        });
     }
-}
-
-    const dateFrom = date?.from !== undefined ? date.from : "Pick a date range";
-    const dateTo = date?.to  !== undefined ? date.to : "Pick a date range";
 
     return (
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                    variant={"outline"}
-                    className={cn(
-                        "w-[240px] justify-start text-left font-normal",
-                        !date && "text-muted-foreground"
-                    )}
-                >
-                    <CalendarIcon/>
-
-                    {date ? `${dateFrom} to ${dateTo}` : <span>Pick a date range</span>}
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="start">
-                <Calendar
-                    mode="range"
-                    selected={date}
-                    onSelect={handleSelect}
-                    initialFocus
-                    className="rounded-md border shadow"
-                />
-            </PopoverContent>
-        </Popover>
+        <div className={"flex"}>
+            <Calendar
+                autoFocus
+                mode="range"
+                selected={selectedRange}
+                onSelect={handleRangeChange}
+                //numberOfMonths={2}
+            />
+        </div>
     );
 }
-
-// return (
-//     <div>
-//         <Calendar
-//             captionLayout="dropdown"
-//             mode="range"
-//             selected={date}
-//             onSelect={handleSelect}
-//             className="p-3 pointer-events-auto rdp-root rounded-md border shadow blue"
-//             max={20}
-//             today={new Date()}
-//             defaultMonth={new Date()}
-//
-//         />
-//     </div>
-// );
